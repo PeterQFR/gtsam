30171a0ca43ce4ffbe33d84af62fb3326fb6e59e
peterQFR <petermilani80@gmail.com>
2025-08-14 10:43:44 +1000

    Add Biased GPS Factor

--- gtsam/navigation/GPSFactor.h
+++ gtsam/navigation/GPSFactor.h
@@ -463,4 +463,95 @@ public:
   }
 };
 
+/**
+ * Prior on position in a Cartesian frame plus a bias for measurement errors
+ * caused by the environment or GPS spoofing.
+ * Possibilities include:
+ *   ENU: East-North-Up navigation frame at some local origin
+ *   NED: North-East-Down navigation frame at some local origin
+ *   ECEF: Earth-centered Earth-fixed, origin at Earth's center
+ * See Farrell08book or e.g. http://www.dirsig.org/docs/new/coordinates.html
+ * @ingroup navigation
+ */
+class GTSAM_EXPORT BiasedGPSFactor: public NoiseModelFactorN<Pose3, Point3> {
+
+private:
+
+  typedef NoiseModelFactorN<Pose3, Point3> Base;
+
+  Point3 nT_; ///< Position measurement in cartesian coordinates (navigation frame)
+
+public:
+
+  // Provide access to the Matrix& version of evaluateError:
+  using Base::evaluateError;
+
+  /// shorthand for a smart pointer to a factor
+  typedef std::shared_ptr<BiasedGPSFactor> shared_ptr;
+
+  /// Typedef to this class
+  typedef BiasedGPSFactor This;
+
+  /** default constructor - only use for serialization */
+  BiasedGPSFactor(): nT_(0, 0, 0) {}
+
+  ~BiasedGPSFactor() override {}
+
+  /**
+   * @brief Constructor from a measurement in a Cartesian frame.
+   * Use GeographicLib to convert from geographic (latitude and longitude) coordinates
+   * @param key of the Pose3 variable that will be constrained
+   * @param gpsIn measurement already in correct coordinates
+   * @param model Gaussian noise model
+   */
+  BiasedGPSFactor(Key keyPose, Key keyBias, const Point3& gpsIn, const SharedNoiseModel& model) :
+      Base(model, keyPose, keyBias), nT_(gpsIn) {
+  }
+
+  /// @return a deep copy of this factor
+  gtsam::NonlinearFactor::shared_ptr clone() const override {
+    return std::static_pointer_cast<gtsam::NonlinearFactor>(
+        gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+  }
+
+  /// print
+  void print(const std::string& s = "", const KeyFormatter& keyFormatter =
+                                            DefaultKeyFormatter) const override;
+
+  /// equals
+  bool equals(const NonlinearFactor& expected, double tol = 1e-9) const override;
+
+  /// vector of errors
+  Vector evaluateError(const Pose3& nTb, const Point3& eM,
+          OptionalMatrixType H, OptionalMatrixType H2) const override;
+
+  /// return the measurement, in the navigation frame
+  inline const Point3 & measurementIn() const {
+    return nT_;
+  }
+
+  /**
+   *  Convenience function to estimate state at time t, given two GPS
+   *  readings (in local NED Cartesian frame) bracketing t
+   *  Assumes roll is zero, calculates yaw and pitch from NED1->NED2 vector.
+   */
+  static std::pair<Pose3, Vector3> EstimateState(double t1, const Point3& NED1,
+      double t2, const Point3& NED2, double timestamp);
+
+private:
+
+#if GTSAM_ENABLE_BOOST_SERIALIZATION  ///
+  /// Serialization function
+  friend class boost::serialization::access;
+  template<class ARCHIVE>
+  void serialize(ARCHIVE & ar, const unsigned int /*version*/) {
+    // NoiseModelFactor1 instead of NoiseModelFactorN for backward compatibility
+    ar
+        & boost::serialization::make_nvp("NoiseModelFactor1",
+            boost::serialization::base_object<Base>(*this));
+    ar & BOOST_SERIALIZATION_NVP(nT_);
+  }
+#endif
+};
+
 } /// namespace gtsam
